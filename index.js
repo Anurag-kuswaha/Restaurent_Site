8c903c80f1dde97e4cd3ac89897e5870f981422f
Anurag Kushwaha <73064862+Anurag-kuswaha@users.noreply.github.com>
2023-02-02 11:15:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const hbs = require('express-handlebars');
 const app = express();
-const port = process.env.PORT || 5010;
+const port = process.env.PORT || 3000;
 const path = require("path");
 var menu = require("./routes/menu");
 
